c319a073ee078081ea0b19c137c41110029fd5c0
Ahmed Kamal <Ahmed-Kamal92@hotmail.com>
2017-12-03 12:04:24 +0200

    - Added unit tests

--- app/src/test/java/ahmed/com/mvp_rx_viewmodel/presentation/questions/QuestionsPresenterTest.java
+++ app/src/test/java/ahmed/com/mvp_rx_viewmodel/presentation/questions/QuestionsPresenterTest.java
@@ -1,11 +1,141 @@
 package ahmed.com.mvp_rx_viewmodel.presentation.questions;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import ahmed.com.mvp_rx_viewmodel.data.model.Question;
+import ahmed.com.mvp_rx_viewmodel.rx.RunOn;
+import ahmed.com.mvp_rx_viewmodel.usecase.QuestionUseCase;
+import io.reactivex.Flowable;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.TestScheduler;
+
+import static ahmed.com.mvp_rx_viewmodel.rx.SchedulerType.IO;
+import static ahmed.com.mvp_rx_viewmodel.rx.SchedulerType.UI;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 /**
  * Created by Ahmed Kamal on 29-11-2017.
  */
 public class QuestionsPresenterTest
 {
+    private static final Question question1 = new Question();
+    private static final Question question2 = new Question();
+    private static final Question question3 = new Question();
+    private List<Question> questions = Arrays.asList(question1, question2, question3);
 
+    @Mock
+    private QuestionUseCase repository;
+
+    @Mock
+    private QuestionsContract.View view;
+
+    @RunOn(IO)
+    private Scheduler ioScheduler;
+
+    @RunOn(UI)
+    private Scheduler uiScheduler;
+
+    private TestScheduler testScheduler;
+
+    private QuestionsPresenter presenter;
+
+    @Before
+    public void setUp() {
+        // Make sure to use TestScheduler for RxJava testing
+        testScheduler = new TestScheduler();
+        ioScheduler = testScheduler;
+        uiScheduler = testScheduler;
+        presenter = new QuestionsPresenter(repository, view, ioScheduler, uiScheduler);
+
+        // Update stub
+        question1.setTitle("question1");
+        question2.setTitle("question2");
+        question3.setTitle("question3");
+    }
+
+    @Test
+    public void loadQuestions_ShouldShowQuestionOnView_WithDataReturned() {
+        // Given
+        doReturn(Flowable.just(questions)).when(repository).loadQuestions(true);
+
+        // When
+        presenter.loadQuestions(true);
+        testScheduler.triggerActions();
+
+        // Then
+        verify(view).clearQuestions();
+        verify(view).showQuestions(questions);
+        verify(view, atLeastOnce()).hideLoading();
+    }
+
+    @Test
+    public void loadQuestions_ShouldShowMessage_WhenNoDataReturned() {
+        // Given
+        doReturn(Flowable.just(new ArrayList<Question>())).when(repository).loadQuestions(true);
+
+        // When
+        presenter.loadQuestions(true);
+        testScheduler.triggerActions();
+
+        // Then
+        verify(view).clearQuestions();
+        verify(view, never()).showQuestions(questions);
+        verify(view).showNoDataMessage();
+        verify(view, atLeastOnce()).hideLoading();
+    }
+
+    @Test
+    public void getQuestion_ShouldShowDetailOnView() {
+        // Given
+        doReturn(Flowable.just(question1)).when(repository).getQuestion(1);
+
+        // When
+        presenter.getQuestion(1);
+        testScheduler.triggerActions();
+
+        // Then
+        verify(view).showQuestionDetail(question1);
+    }
+
+    @Test
+    public void search_ResultShouldBeShownOnView_WhenFilteredDataIsNotEmpty() {
+        // Given
+        doReturn(Flowable.just(questions)).when(repository).loadQuestions(false);
+
+        // When
+        presenter.search("question1");
+        testScheduler.triggerActions();
+
+        // Then
+        // Return a list of questions which should contains only question 1.
+        verify(view).showQuestions(Collections.singletonList(question1));
+        verifyNoMoreInteractions(view);
+    }
+
+    @Test
+    public void search_EmptyMessageShouldBeShownOnView_WhenFilteredDataIsEmpty() {
+        // Given
+        doReturn(Flowable.just(questions)).when(repository).loadQuestions(false);
+
+        // When
+        presenter.search("invalid question title");
+        testScheduler.triggerActions();
+
+        // Then
+        verify(view).clearQuestions();
+        verify(view).showEmptySearchResult();
+        verifyNoMoreInteractions(view);
+    }
 }
--- app/src/test/java/ahmed/com/mvp_rx_viewmodel/usecase/QuestionUseCaseTest.java
+++ app/src/test/java/ahmed/com/mvp_rx_viewmodel/usecase/QuestionUseCaseTest.java
@@ -1,11 +1,122 @@
 package ahmed.com.mvp_rx_viewmodel.usecase;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ahmed.com.mvp_rx_viewmodel.data.model.Question;
+import ahmed.com.mvp_rx_viewmodel.usecase.local.Local;
+import ahmed.com.mvp_rx_viewmodel.usecase.remote.Remote;
+import io.reactivex.Flowable;
+import io.reactivex.subscribers.TestSubscriber;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
 /**
  * Created by Ahmed Kamal on 29-11-2017.
  */
 public class QuestionUseCaseTest
 {
+    private static final Question question1 = new Question();
+    private static final Question question2 = new Question();
+    private static final Question question3 = new Question();
+    private List<Question> questions = Arrays.asList(question1, question2, question3);
 
+    @Mock
+    @Local
+    private QuestionDataSource localDataSource;
+
+    @Mock
+    @Remote
+    private QuestionDataSource remoteDataSource;
+
+    private QuestionUseCase questionUseCase;
+
+    private TestSubscriber<List<Question>> questionsTestSubscriber;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        questionUseCase = new QuestionUseCase(localDataSource, remoteDataSource);
+
+        questionsTestSubscriber = new TestSubscriber<>();
+    }
+
+    @Test
+    public void loadQuestions_ShouldReturnCache_IfItIsAvailable() {
+        // Given
+        questionUseCase.caches = questions;
+
+        // When
+        questionUseCase.loadQuestions(false).subscribe(questionsTestSubscriber);
+
+        // Then
+        // No interaction with local storage or remote source
+        verifyZeroInteractions(localDataSource);
+        verifyZeroInteractions(remoteDataSource);
+
+        questionsTestSubscriber.assertValue(questions);
+    }
+
+    @Test
+    public void loadQuestions_ShouldReturnFromLocal_IfCacheIsNotAvailable() {
+        // Given
+        // No cache
+        questionUseCase.caches.clear();
+        doReturn(Flowable.just(questions)).when(localDataSource).loadQuestions(false);
+        doReturn(Flowable.just(questions)).when(remoteDataSource).loadQuestions(true);
+
+        // When
+        questionUseCase.loadQuestions(false).subscribe(questionsTestSubscriber);
+
+        // Then
+        // Loads from local storage
+        verify(localDataSource).loadQuestions(false);
+        // Will load from remote source if there is no local data available
+        verify(remoteDataSource).loadQuestions(true);
+
+        questionsTestSubscriber.assertValue(questions);
+    }
+
+    @Test
+    public void loadQuestions_ShouldReturnFromRemote_WhenItIsRequired() {
+        // Given
+        questionUseCase.caches.clear();
+        doReturn(Flowable.just(questions)).when(remoteDataSource).loadQuestions(true);
+
+        // When
+        questionUseCase.loadQuestions(true).subscribe(questionsTestSubscriber);
+
+        // Then
+        // Load from remote not from local storage
+        verify(remoteDataSource).loadQuestions(true);
+        verify(localDataSource, never()).loadQuestions(true);
+        // Cache and local storage data are clear and are filled with new data
+        verify(localDataSource).clearData();
+        assertEquals(questionUseCase.caches, questions);
+        verify(localDataSource).addQuestion(question1);
+        verify(localDataSource).addQuestion(question2);
+        verify(localDataSource).addQuestion(question3);
+
+        questionsTestSubscriber.assertValue(questions);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void addQuestion_ShouldThrowException() {
+        questionUseCase.addQuestion(question1);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void clearData_ShouldThrowException() {
+        questionUseCase.clearData();
+    }
 }
